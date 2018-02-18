1c1b162b1cb42af16d6bf4edaaac4cf8a8e14ae2
Robert Collewijn <robert@tooconnect.nl>
2018-02-18 08:17:21 +0100

    INIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from "./app.material";
 
 
 @NgModule({
@@ -10,8 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
   ],
+  exports: [MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
